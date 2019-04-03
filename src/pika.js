d2d8bbd64b35188a5d3f7a9cd3b400847351f345
xiaoshen.zl <xiaoshen.zl@alibaba-inc.com>
2019-04-03 19:30:55 +0800

    first commit

---
+++ src/pika.js
@@ -0,0 +1,4 @@
+import { Random } from "/web_modules/random-js.js";
+
+const random = new Random();
+console.log(random.integer(1, 100));
---
+++ src/webpack.js
@@ -0,0 +1,4 @@
+import { Random } from "random-js";
+
+const random = new Random();
+console.log(random.integer(1, 100));
